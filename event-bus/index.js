57ed05cb02bea203ee0d2ae38a1e61458de37fea
Glenn Pinto <gvpinto@gmail.com>
2023-05-01 22:58:31 -0400

    trigger github actions

--- event-bus/index.js
+++ event-bus/index.js
@@ -13,6 +13,7 @@ app.post('/events', (req, res) => {
     events.push(event);
     axios.post('http://posts-srv:4000/events', event).catch((err) => console.log(err.message));
     // axios.post('http://localhost:4001/events', event).catch((err) => console.log(err.message));
+
     // axios.post('http://localhost:4002/events', event).catch((err) => console.log(err.message));
     // axios.post('http://localhost:4003/events', event).catch((err) => console.log(err.message));
 
